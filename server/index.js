8fcca8dc520eee94c2afc5270386ae315cc88e38
caibao111 <651246074@qq.com>
2020-08-04 16:10:16 +0800

    添加一个服务

---
+++ server/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: caist
+ * @Date: 2020-08-04 16:04:20
+ * @LastEditTime: 2020-08-04 16:09:02
+ * @LastEditors: Please set LastEditors
+ * @Description: entry port
+ * @FilePath: \douban-trailer\server\index.js
+ */
+const Koa = require('koa')
+const logger = require('koa-logger')
+
+const app = new Koa()
+
+app.use(logger())
+app.use(async (ctx, next) => {
+  ctx.body = 'Hi Luke'
+})
+
+app.listen(4455)
